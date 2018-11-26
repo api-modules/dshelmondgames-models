3ac8c30003d95c609b696a48dd6006f93c07eb8b
Duncan Sterken <duncan.sterken123@gmail.com>
2018-11-26 10:21:46 +0100

    Create migration for custom commands

---
+++ Bot/CustomCommand.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Bot;
+
+use App\Models\BotModelBase;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CustomCommand extends BotModelBase
+{
+    use SoftDeletes;
+
+    protected $table = 'customCommands';
+}
