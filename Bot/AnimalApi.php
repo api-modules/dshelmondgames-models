75e9298619c02b22e5ec67413c6f7a768e9f236b
Duncan Sterken <duncan.sterken123@gmail.com>
2019-01-07 13:45:45 +0100

    Make part of the api public

--- Bot/AnimalApi.php
+++ Bot/AnimalApi.php
@@ -5,6 +5,27 @@ namespace App\Models\Bot;
 use App\Models\BotModelBase;
 use Illuminate\Database\Eloquent\Builder;
 
+/**
+ * Class AnimalApi.
+ *
+ * @OA\Schema(
+ *   type="object",
+ *   @OA\Xml(name="AnimalApi")
+ * )
+ *
+ * @OA\Property(
+ *   property="id",
+ *   type="integer",
+ *   description="The animal id",
+ *   example=1
+ * ),
+ * @OA\Property(
+ *   property="file",
+ *   type="string",
+ *   description="The the location of the image",
+ *   example="https://example.com/example.png"
+ * )
+ */
 class AnimalApi extends BotModelBase
 {
     protected $hidden = [
