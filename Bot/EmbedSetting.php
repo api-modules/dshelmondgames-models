ccc1d20a9f3960d6b770f4d4d58d2faee4226788
Duncan Sterken <duncan.sterken123@gmail.com>
2019-01-08 15:01:07 +0100

    Todo: Moderation and patrons

--- Bot/EmbedSetting.php
+++ Bot/EmbedSetting.php
@@ -5,6 +5,27 @@ namespace App\Models\Bot;
 use App\Models\BotModelBase;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * Class EmbedSetting.
+ *
+ * @OA\Schema(
+ *   type="object",
+ *   @OA\Xml(name="EmbedSetting")
+ * )
+ *
+ * @OA\Property(
+ *   property="guild_id",
+ *   type="string",
+ *   description="The uniqe id of this item that also holds the id of the target guild",
+ *   example="1"
+ * ),
+ * @OA\Property(
+ *   property="embed_color",
+ *   type="integer",
+ *   description="The custom color for the embed",
+ *   example=1
+ * ),
+ */
 class EmbedSetting extends BotModelBase
 {
     use SoftDeletes;
--- Bot/Pronoun.php
+++ Bot/Pronoun.php
@@ -4,6 +4,33 @@ namespace App\Models\Bot;
 
 use App\Models\BotModelBase;
 
+/**
+ * Class Pronoun.
+ *
+ * @OA\Schema(
+ *   type="object",
+ *   @OA\Xml(name="Pronoun")
+ * )
+ *
+ * @OA\Property(
+ *   property="user_id",
+ *   type="string",
+ *   description="The id of the user that this pronoun is for",
+ *   example="1"
+ * ),
+ * @OA\Property(
+ *   property="pronouns",
+ *   type="string",
+ *   description="The pronouns of this user",
+ *   example="he/him/his/his"
+ * ),
+ * @OA\Property(
+ *   property="singular",
+ *   type="boolean",
+ *   description="If this pronoun is singular",
+ *   example=true
+ * ),
+ */
 class Pronoun extends BotModelBase
 {
     protected $primaryKey = 'user_id';
