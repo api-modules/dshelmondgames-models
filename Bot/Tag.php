2622032c76c9eb11de675dd89af6b764e84fb993
duncte123 <contact@duncte123.me>
2020-10-27 08:39:02 +0100

    Add some documentation

--- Bot/Tag.php
+++ Bot/Tag.php
@@ -14,6 +14,36 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @property string  owner_id
  * @property string  name
  * @property string  content
+ *
+ * @OA\Schema(
+ *   type="object",
+ *   @OA\Xml(name="Tag")
+ * )
+ *
+ * @OA\Property(
+ *   property="id",
+ *   type="integer",
+ *   description="The uniqe id of this item",
+ *   example=9
+ * ),
+ * @OA\Property(
+ *   property="owner_id",
+ *   type="string",
+ *   description="The id of the user that created this tag",
+ *   example="191245668617158656"
+ * ),
+ * @OA\Property(
+ *   property="name",
+ *   type="string",
+ *   description="The unique name of this tag",
+ *   example="cheese"
+ * ),
+ * @OA\Property(
+ *   property="content",
+ *   type="string",
+ *   description="The content of this tag",
+ *   example="Hello World!"
+ * ),
  */
 class Tag extends BotModelBase
 {
